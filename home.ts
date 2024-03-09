e505dab97c3d3be30ecc6c7a4748f2a886514bf4
AmnaSheikh15 <161419330+AmnaSheikh15@users.noreply.github.com>
2024-03-09 17:25:54 +0500

    Add files via upload

---
+++ home.ts
@@ -0,0 +1,23 @@
+// task 1 "String" Data type 
+let FavDish: string = "biryani"
+let FastFood: string = "Pizza"
+console.log("i love" + FavDish  + " i like"  + FastFood);
+
+// task2  Arithmetic data type
+let a = 10;
+let b = 15;
+console.log("a = ", a, "& b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b)
+console.log("a / b=", a / b);
+console.log("a % b = ", a % b);
+
+// task 3 boolean data type 
+let tb : boolean = true;
+console.log(tb);
+let fb : boolean = false;
+console.log(fb)
+let nb :null = null;  
+console.log(nb);
+
