0eb316e010e1e9e6026cd5ae35f564e1363a341f
AmnaSheikh15 <161419330+AmnaSheikh15@users.noreply.github.com>
2024-03-09 19:26:33 +0500

    Add files via upload

---
+++ home2.ts
@@ -0,0 +1,37 @@
+// camel case
+let  myName :string = "amna Adnan";
+console.log(myName);
+// snake case
+let my_cast :string = "amna_sheikh";
+console.log(my_cast);
+// pascal case 
+let ILiveIn :string = "Karachi,Pakistan";
+console.log(ILiveIn);
+
+// Arrays and their types
+// string 
+let Name : string[] = ["Apple", "Mango","Banana","Graps","Gava"];
+console.log(Name[0]);
+console.log(Name[1],Name[2]);
+console.log(Name[2]);
+console.log(Name[3]);
+console.log(Name[4]);
+
+// Number Array 
+let number: number[] = [10, 20, 30 , 40 , 50 ];
+console.log(number[0]);
+console.log(number[1],number[4] );
+console.log(number[2]);
+console.log(number[3],number[0]);
+console.log(number[4]);
+
+
+// bolean Array 
+let bol: boolean[] = [true,false,true,false,true];
+console.log(bol[0]);
+console.log(bol[1]);
+console.log(bol[2]);
+console.log(bol[3]);
+console.log(bol[4]);
+console.log(bol[2],bol[3]);
+console.log(bol[0],bol[2], bol[3],bol[4]);
